683e3b0332f58e75fd4add534074a8c3741a3f7a
Anushka <anushkamar1102@gmail.com>
2023-12-13 11:59:27 +0530

    created address book service db

---
+++ src/com/bridgeLabz/practiceproblem/address_book.sql
@@ -0,0 +1,19 @@
+-- welcome to address_book Database
+-- DAY16 practical problem
+
+--UC - 1
+--creating the database
+create database address_book;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| address_book       |
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sys                |
++--------------------+
